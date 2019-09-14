88d8fe5f6cbd5c0bc2c3fbbecd33b9dc9ee1c6cc
Maxwell Alexius <destinydragon85614@gmail.com>
2019-09-14 23:17:30 +0800

    Day 9 Complete

--- 01-basics/index.ts
+++ 01-basics/index.ts
@@ -540,3 +540,52 @@ let infoAboutMartin = {
 };
 
 printInfo(infoAboutMartin);
+
+
+
+
+
+
+/* ------------------------------- Day 09 ----------------------------------- */
+
+enum Gender { Male, Female, Other };
+
+type TestAccountInfo = {
+  account: string,
+  password: string,
+  nickname: string | undefined,
+  birth: Date | undefined,
+  gender: Gender | undefined,
+  subscribed: boolean
+};
+
+// 依然出錯！
+// let accountMaxwell: TestAccountInfo = {
+//   account: 'nordic.wyvern',
+//   password: '<hashed-password>',
+//   subscribed: false
+// };
+
+type AccountInfo = {
+  account: string,
+  password: string,
+  nickname?: string,
+  birth?: Date,
+  gender?: Gender,
+  subscribed: boolean
+};
+
+// let accountMaxwell: AccountInfo = {
+//   account: 'nordic.wyvern',
+//   password: '<hashed-password>',
+//   subscribed: false
+// };
+
+let additionThreeAsDefault = function (num1: number, num2?: number) {
+  if (num2) {
+    return num1 + num2;
+  }
+  return num1 + 3;
+}
+
+type VehicleInfoWithOptionalElements = [string, string, string?, Date?];
