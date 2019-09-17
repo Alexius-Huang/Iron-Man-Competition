bbce0e0df066607f1d895b1dd74c964a02a4b3c6
Maxwell Alexius <destinydragon85614@gmail.com>
2019-09-17 20:50:06 +0800

    Day 15 Complete

--- 02-interface-class/index.ts
+++ 02-interface-class/index.ts
@@ -371,3 +371,176 @@ interface MyDocument {
 //     query?: Dictionary;
 //   }
 // }
+
+
+
+
+
+
+/* ------------------------------- Day 15 ----------------------------------- */
+
+/* Indexable Types */
+// 定義一種物件型別 Dictionary，其中，它的鍵值對都是字串型態
+type Dictionary = {
+  [propName: string]: string,
+};
+
+// 定義類似陣列的型別，其裡面儲存的值為字串
+interface StringTypedList {
+  [index: number]: string;
+}
+
+// Dictionary 範例
+// 正常使用方式
+let normalDictionary: Dictionary = {
+  hello: 'world',
+  thisFeature: 'is crazy'
+};
+
+// 空的狹義物件狀態也行
+let emptyDictionary: Dictionary = {};
+
+// 錯誤的使用方式
+// let wrongDictionary: Dictionary = {
+//   hello: 123,
+//   thisFeature: true,
+//   withFunction() { console.log('Wrong type!'); },
+//   nestedDictionary: { hello: 123 },
+// };
+
+// StringTypedList 範例
+// 正常使用方式
+let stringTypedArray: StringTypedList = {
+  123: 'Hello',
+  [456]: 'Hi',
+};
+
+// 空的狹義物件狀態也行
+let emptyStringTypedArray: StringTypedList = {};
+
+// 但不可以直接變成 Array
+// let stringTypedArrayLiteral: StringTypedList = [1, 2, 3];
+
+// 但卻可以為空的 Array
+let emptyStringTypedArray2: StringTypedList = [];
+
+// 可以用數字進行索引
+stringTypedArray[0];
+stringTypedArray[1];
+
+// 錯誤使用方式
+// let wrongStringTypedArray: StringTypedList = {
+//   message: 'Hello',
+//   thisFeature: true
+// };
+
+// 也當然不能被字串索引
+// stringTypedArray['hello'];
+
+// 當然更不可能用點來呼叫屬性，因為會被當成字串
+// stringTypedArray.hello;
+
+/* 選用屬性 Optional Properties */
+type TAccountUser = {
+  email: string,
+  password: string,
+  name?: string,
+  age?: number,
+};
+
+interface IAccountUser {
+  email: string;
+  password: string;
+  name?: string;
+  age?: number;
+}
+
+/* 唯讀屬性 */
+type TAccountUserWithReadonlyProperty = {
+  readonly email: string,
+  readonly password: string,
+  name?: string,
+  age?: number,
+};
+
+interface IAccountUserWithReadonlyProperty {
+  readonly email: string;
+  readonly password: string;
+  name?: string;
+  age?: number;
+}
+
+let sampleAccount: TAccountUserWithReadonlyProperty = {
+  email: 'maxwell@example.com',
+  password: '<hashed-password>',
+  name: 'Maxwell'
+};
+
+// 可以讀取
+sampleAccount.email;
+
+// 但不能寫入 => 強行寫入就被 TS 警告
+// sampleAccount.email = 'new@example.com';
+
+/* 混合型介面 Hybrid Type Interface */
+interface Counter {
+  // 純函式格式
+  (start: number): void;
+
+  // 狹義物件的格式
+  increment(): number;
+  reset(): void;
+  value: number;
+}
+
+function createCounter(): Counter {
+  let value: number;
+  let initializedNumber: number;
+
+  // 實踐純函式的部分
+  const counter = (function (startNumber: number) {
+    initializedNumber = startNumber;
+    value = startNumber;
+  }) as Counter;
+
+  // 實踐狹義物件格式的部分
+  counter.increment = function() {
+    value++;
+    return value;
+  };
+
+  counter.reset = function() {
+    value = initializedNumber;
+  };
+
+  Object.defineProperty(counter, 'value', {
+    get() { return value; }
+  });
+
+  return counter;
+}
+
+// 建立一個 counter 物件
+const counter: Counter = createCounter();
+
+// 藉由 Counter 介面裡面中，純函式型別裡的格式：
+// (start: number): void
+// 我們可以填入數字
+counter(5); // <- 初始化值為 5
+
+// 呼叫 Counter 介面裡的 value 屬性
+console.log(counter.value); // 應該要得出 5
+
+// 呼叫 3 次 Counter 介面裡的 increment 方法
+counter.increment();
+counter.increment();
+counter.increment();
+
+// 再呼叫一次 Counter 介面裡的 value 屬性
+console.log(counter.value); // 應該要得出 8
+
+// 呼叫 Counter 介面裡的 reset 方法
+counter.reset();
+
+// 再呼叫一次 Counter 介面裡的 value 屬性
+console.log(counter.value); // 應該要得出 5，也就是原本的初始值
