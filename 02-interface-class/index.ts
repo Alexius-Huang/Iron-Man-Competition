7968a5b35aae5d0807584f234b270a1931a9b2cf
Maxwell Alexius <destinydragon85614@gmail.com>
2019-09-18 14:59:41 +0800

    Day 16 Complete

--- 02-interface-class/index.ts
+++ 02-interface-class/index.ts
@@ -544,3 +544,44 @@ counter.reset();
 
 // 再呼叫一次 Counter 介面裡的 value 屬性
 console.log(counter.value); // 應該要得出 5，也就是原本的初始值
+
+
+
+
+
+
+/* ------------------------------- Day 16 ----------------------------------- */
+
+/* 介面不能單純用原始型別表示 */
+// 根本沒有介面這樣的定義行為，實質上也沒意義啊
+// interface I {
+//   number;
+// }
+
+// 遑論單純複合狀態
+// interface J {
+//   number | string;
+// }
+
+/* Duck-typing 模式：建議函式之參數註記為介面 */
+interface PersonInfo {
+  name: string;
+  age: number;
+  hasPet: boolean;
+}
+
+function logPersonInfo (person: PersonInfo): void {
+  console.log(`Name: ${person.name}`);
+  console.log(`Age: ${person.age}`);
+  console.log(`Owns a pet? ${person.hasPet}`);
+}
+
+let maxwellInfo = {
+  name: 'Maxwell',
+  age: 20,
+  hasPet: false,
+  email: 'maxwell@example.com',
+  ownsMotorcycle: false,
+};
+
+logPersonInfo(maxwellInfo);
